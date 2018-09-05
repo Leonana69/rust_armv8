941a7615b21d77928a3114ffcb7c94b4f50066fc
leonana69 <leonana69@github.com>
2018-09-05 17:34:10 -0500

    update print module

--- armv8-rust/src/print.rs
+++ armv8-rust/src/print.rs
@@ -1,8 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_macros)]
-// use volatile::Volatile;
-// use core::fmt;
-// use spin::Mutex;
+
 use reg;
 
 pub fn puts(s: &str) {
@@ -14,26 +12,6 @@ pub fn puts(s: &str) {
 pub fn putchar(c: char) {
 	unsafe { *(reg::UART_BASE as *mut u32) = c as u32; };
 }
-
-macro_rules! puts {
-	($($arg: tt)*) => (print::puts($($arg)*))
-}
-
-macro_rules! putchar {
-	($($arg: tt)*) => (print::putchar($($arg)*))
-}
-
-macro_rules! print {
-	() => (puts!("\n"));
-    ($fmt:expr) => (puts!($fmt));
-}
-
-macro_rules! println {
-	() => (puts!("\n"));
-    ($fmt:expr) => (puts!(concat!($fmt, "\n")));
-}
-
-
 
 pub fn putu32(u: u32) {
 	if u == 0 {
@@ -84,6 +62,25 @@ pub fn putu64(u: u64) {
 	}
 }
 
+macro_rules! puts {
+	($($arg: tt)*) => (print::puts($($arg)*))
+}
+
+macro_rules! putchar {
+	($($arg: tt)*) => (print::putchar($($arg)*))
+}
+
+macro_rules! print {
+	() => (puts!("\n"));
+    ($fmt:expr) => (puts!($fmt));
+}
+
+macro_rules! println {
+	() => (puts!("\n"));
+    ($fmt:expr) => (puts!(concat!($fmt, "\n")));
+}
+
+
 macro_rules! printu32 {
 	() => (puts!("\n"));
     ($fmt:expr) => (print::putu32($fmt));
@@ -93,73 +90,3 @@ macro_rules! printu64 {
 	() => (puts!("\n"));
     ($fmt:expr) => (print::putu64($fmt));
 }
-// pub struct Buffer {
-// 	pub chars: Volatile<u8>,
-// }
-
-// pub struct Writer {
-//     pub out_id: u32,
-//     // pub buffer: &'static mut Buffer,
-// }
-
-// impl Writer {
-//     pub fn write_byte(&mut self, byte: u8) {
-//         unsafe { *(reg::UART_BASE as *mut u32) = byte as u32; };
-//     }
-//     pub fn write_string(&mut self, s: &str) {
-//         for byte in s.bytes() {
-//         	match byte {
-//                 // printable ASCII byte or newline
-//                 0x20...0x7e | b'\n' => self.write_byte(byte),
-//                 // not part of printable ASCII range
-//                 _ => self.write_byte(0xfe),
-//             }
-//         }
-//     }
-// }
-
-// impl fmt::Write for Writer {
-
-//     fn write_str(&mut self, s: &str) -> fmt::Result {
-//         self.write_string(s);
-//         Ok(())
-//     }
-
-//     fn write_char(&mut self, c: char) -> fmt::Result {
-//     	self.write_byte(c as u8);
-//     	Ok(())
-//     }
-// }
-
-// lazy_static! {
-//     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-//         out_id: 1,
-//         buffer: unsafe { &mut *(reg::UART_BASE as *mut Buffer) },
-//     });
-// }
-
-// macro_rules! print {
-//     ($($arg:tt)*) => ($crate::print::print(format_args!($($arg)*)));
-// }
-
-// pub fn print(args: fmt::Arguments) {
-//     use core::fmt::Write;
-//     WRITER.lock().write_fmt(args).unwrap();
-// }
-
-// macro_rules! writee {
-// 	($dst: expr, $($arg: tt)*) => ($dst.write_fmt(format_args!($($arg)*)))
-// }
-// macro_rules! print {
-//     ($($arg:tt)*) => ({
-//     	use core::fmt::Write;
-//     	let mut writer = print::Writer{out_id: 0,};
-//     	format!($($arg)*);
-//     });
-// }
-
-// macro_rules! println {
-//     ($fmt:expr) => (print!(concat!($fmt, "\n")));
-//     ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
-// }
-
--- armv8-rust/src/reg.rs
+++ armv8-rust/src/reg.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)]
 
+// UART0 addr is 0x9000000
 pub const UART_BASE: u32 = 0x09000000 as u32;
